aca92cf92baa561ed811bc94b1d747f1da39fb93
vinuc49 <vinuchandregowda@gmail.com>
2023-12-27 12:07:41 +0000

    home page

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from blog.views import my_blog
 
 urlpatterns = [
+    path('', my_blog, name='blog'),
     path('blog/', my_blog, name='blog'),
     path('admin/', admin.site.urls),
 ]
