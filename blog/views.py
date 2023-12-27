3479a593e13018293d7f7ec351b6507a06bd88c7
vinuc49 <vinuchandregowda@gmail.com>
2023-12-27 23:58:58 +0000

    add base and index HTML

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,5 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.all().order_by("created_on")
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
