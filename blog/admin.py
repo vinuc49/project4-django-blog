c5f7f8b195e95e8de5a9d49d52d250c0978fd68c
vinuc49 <vinuchandregowda@gmail.com>
2023-12-27 22:17:03 +0000

    data load

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,9 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     content = models.TextField()
+    excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
