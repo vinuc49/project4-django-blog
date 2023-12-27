bc81af799f3b8cd5b887240064b340bfa5bcc137
vinuc49 <vinuchandregowda@gmail.com>
2023-12-27 12:52:12 +0000

    ADDED  SECREAT KEY AS AN EXAMPLE

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ok65s%ov$#5$0=dw%gxw2%dpmosnl$2(0_&ombuz^$h$l%l^i*'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
